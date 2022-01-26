51c693a3144f4813e132ad200576f692c4fbd43c
gustavo-gamboa <gustavo.gamboa@allianzdirect.es>
2022-01-26 17:11:59 +0100

    tslint instalado y funcionando

--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -1,17 +1,19 @@
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { catchError, Observable, throwError } from 'rxjs';
-import { Weather, WeatherData } from '../data/weather-data';
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { catchError, Observable, throwError } from "rxjs";
+import { WeatherData } from "../data/weather-data";
 
-const url = "http://api.openweathermap.org/data/2.5/weather?"
+const url = "http://api.openweathermap.org/data/2.5/weather?";
 const queryCityName = "q=";
 const queryLang = "&lang=es";
-const appid = "&appid=c2395dafd752b24690e1cdd50b5a6972"
+const appid = "&appid=c2395dafd752b24690e1cdd50b5a6972";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class WeatherService {
+
+  message: any = "";
 
   constructor(private httpClient: HttpClient) { }
 
@@ -19,15 +21,15 @@ export class WeatherService {
     const completeUrl = url + queryCityName+cityName + queryLang + appid;
     return this.httpClient
       .get<WeatherData>(completeUrl)
-      .pipe(catchError(this.handleError))
+      .pipe(catchError(this.handleError));
   }
 
   private handleError(error: HttpErrorResponse) {
     if(error.status == 400) {
-      console.error("Ha ocurrido un error tipo: ", error.status)
+      console.error("Ha ocurrido un error tipo: ", error.status);
     } else {
-      console.error("Otro tipo de error: ", error.message)
+      console.error("Otro tipo de error: ", error.message);
     }
-    return throwError(() => new Error("Algo fue mal..."))
+    return throwError(() => new Error("Algo fue mal..."));
   }
 }
